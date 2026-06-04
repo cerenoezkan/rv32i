1a5dd7a7def6e3f2ab2bff90dd68edcf8a7b6bac
sumeyyegull <gulsumeyye872@gmail.com>
2026-06-04 17:20:02 +0300

    test1 uzerinde yapılan guncelleme

--- test1_main.asm
+++ test1_main.asm
@@ -1,37 +1,24 @@
-# test_program_1.asm
-# UART '1' -> all 6 LEDs on
-# UART '0' -> arithmetic result
-
 .globl MAIN
-
 .text
 
 MAIN:
-    addi x1, x0, 8
-    addi x2, x0, 3
-
-    # Fonksiyon parametreleri
-    add  x10, x1, x0
-    add  x11, x2, x0
+    addi x10, x0, 8
+    addi x11, x0, 3
 
     jal  x1, ADD_FUNC
 
-    # x12 = 11
-    add  x3, x12, x0
+    sub  x12, x12, x11
+    sub  x12, x12, x11
 
-    sub  x4, x3, x2
-    sub  x5, x4, x2      # x5 = 5
+    lui  x6, 2
+    addi x7, x6, 8
+    addi x8, x6, 12
 
-    lui  x6, 2           # x6 = 0x2000 LED
-    addi x7, x6, 8       # x7 = 0x2008 UART data
-    addi x8, x6, 12      # x8 = 0x200C UART ready
-
-    sw   x5, 0(x6)
+    sw   x12, 0(x6)
 
 FLUSH:
     lw   x9, 0(x8)
     beq  x9, x0, LOOP
-
     lw   x10, 0(x7)
     jal  x0, FLUSH
 
@@ -48,10 +35,10 @@ LOOP:
     jal  x0, LOOP
 
 ALL_ON:
-    addi x12, x0, 63
-    sw   x12, 0(x6)
+    addi x9, x0, 63
+    sw   x9, 0(x6)
     jal  x0, LOOP
 
 ARITH:
-    sw   x5, 0(x6)
+    sw   x12, 0(x6)
     jal  x0, LOOP
